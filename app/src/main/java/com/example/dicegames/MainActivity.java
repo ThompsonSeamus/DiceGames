803d8decc8624e07b7df0460d6583688b65f284e
seamu.th2363 <seamu.th2363@vusd.us>
2022-10-28 15:28:12 -0700

    Initialized sounds TODO logo and everything else

--- app/src/main/java/com/example/dicegames/MainActivity.java
+++ app/src/main/java/com/example/dicegames/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.dicegames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.MediaController;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaController mediaController;
+    private MediaPlayer no, rockAndRoll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        no = no.create(this, R.raw.nooooo);
+        rockAndRoll = rockAndRoll.create(this, R.raw.rock_and_roll);
     }
 }
