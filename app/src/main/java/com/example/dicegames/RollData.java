7ce655109a72061df200c64343f455165b48efa2
seamu.th2363 <seamu.th2363@vusd.us>
2022-11-07 15:21:36 -0800

    TODO get information between fragments

--- app/src/main/java/com/example/dicegames/RollData.java
+++ app/src/main/java/com/example/dicegames/RollData.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,6 +60,22 @@ public class RollData extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
         rollNumET = rollNumET.findViewById(R.id.editTextNumber);
+        rollNumET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Toast.makeText(getContext(), "Text Changed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 
     @Override
