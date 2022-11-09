2dab2bc5815102562e99e53252ce6e4c6aeef6ee
seamu.th2363 <seamu.th2363@vusd.us>
2022-11-09 15:28:34 -0800

    Fixed Activities TODO fix transition between activities

---
+++ app/src/main/java/com/example/dicegames/Die.java
@@ -0,0 +1,45 @@
+package com.example.dicegames;
+
+public class Die {
+
+    private int sides;
+    private int cheating;
+
+    //constructors
+    public Die(){
+        this.sides = 6;
+        this.cheating = 0;
+    }
+
+    public Die(int sides, int cheating) {
+        this.sides = sides;
+        this.cheating = cheating;
+    }
+
+    public int roll(){
+        //the number
+        int cheatRolledNum = (int)(Math.random()*101);
+        if(cheatRolledNum <= cheating){
+            return sides;
+        }
+        return (int)(Math.random()*sides) + 1;
+    }
+
+    //setters
+    public void setSides(int sides) {
+        this.sides = sides;
+    }
+
+    public void setCheating(int cheating) {
+        this.cheating = cheating;
+    }
+
+    //getters
+    public int getSides() {
+        return sides;
+    }
+
+    public int getCheating() {
+        return cheating;
+    }
+}
--- app/src/main/java/com/example/dicegames/MainActivity.java
+++ app/src/main/java/com/example/dicegames/MainActivity.java
@@ -3,21 +3,32 @@ package com.example.dicegames;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 
 public class MainActivity extends AppCompatActivity {
-
-    private MediaController mediaController;
-    private MediaPlayer no, rockAndRoll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        no = no.create(this, R.raw.nooooo);
-        rockAndRoll = rockAndRoll.create(this, R.raw.rock_and_roll);
+        Button toRollingActivityButton = findViewById(R.id.to_rolling_button);
+        toRollingActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivity();
+            }
+        });
+    }
+
+    //passing a JSON to get an object
+    public void switchActivity(){
+        Intent rollIntent = new Intent(this, RollingDice.class);
+        startActivity(rollIntent);
     }
 }
