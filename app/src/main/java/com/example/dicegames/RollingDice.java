305b83633ca1d715f4b406ade8485e61120e49b6
seamu.th2363 <seamu.th2363@vusd.us>
2022-12-09 15:27:43 -0800

    TODO Make some way of knowing what number your roll is out of.

--- app/src/main/java/com/example/dicegames/RollingDice.java
+++ app/src/main/java/com/example/dicegames/RollingDice.java
@@ -32,6 +32,9 @@ public class RollingDice extends Activity {
     private boolean shaking = false;
     private boolean atRest = true;
 
+    MediaPlayer rockAndRoll,no,cheatedD20,cheatedD6,d100, d2, d10r1, d10r2, d10r3,d12r2,d20_roll2,
+            d20_roll1,d20_roll3,d12r3,d4r2,d6r1,d6r2,d8r1,d8r2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,25 +50,25 @@ public class RollingDice extends Activity {
         cheat = fromRollDataIntent.getIntExtra("cheat", cheat);
 
         //Media Player Things
-        MediaPlayer rockAndRoll = MediaPlayer.create(this, R.raw.rock_and_roll);
-        MediaPlayer no = MediaPlayer.create(this, R.raw.nooooo);
-        MediaPlayer cheatedD20 = MediaPlayer.create(this, R.raw.cheatd20);
-        MediaPlayer cheatedD6 = MediaPlayer.create(this, R.raw.cheatd6);
-        MediaPlayer d100 = MediaPlayer.create(this, R.raw.d100);
-        MediaPlayer d2 = MediaPlayer.create(this, R.raw.d2);
-        MediaPlayer d10r1 = MediaPlayer.create(this, R.raw.d10r1);
-        MediaPlayer d10r2 = MediaPlayer.create(this, R.raw.d10r2);
-        MediaPlayer d10r3 = MediaPlayer.create(this, R.raw.d10r3);
-        MediaPlayer d12r2 = MediaPlayer.create(this, R.raw.d12r2);
-        MediaPlayer d12r3 = MediaPlayer.create(this, R.raw.d12r3);
-        MediaPlayer d20_roll1 = MediaPlayer.create(this, R.raw.d20_roll1);
-        MediaPlayer d20_roll2 = MediaPlayer.create(this, R.raw.d20_roll2);
-        MediaPlayer d20_roll3 = MediaPlayer.create(this, R.raw.d20_roll3);
-        MediaPlayer d4r2 = MediaPlayer.create(this, R.raw.d4r2);
-        MediaPlayer d6r1 = MediaPlayer.create(this, R.raw.d6r1);
-        MediaPlayer d6r2 = MediaPlayer.create(this, R.raw.d6r2);
-        MediaPlayer d8r1 = MediaPlayer.create(this, R.raw.d8r1);
-        MediaPlayer d8r2 = MediaPlayer.create(this, R.raw.d8r2);
+        rockAndRoll = MediaPlayer.create(this, R.raw.rock_and_roll);
+        no = MediaPlayer.create(this, R.raw.nooooo);
+        cheatedD20 = MediaPlayer.create(this, R.raw.cheatd20);
+        cheatedD6 = MediaPlayer.create(this, R.raw.cheatd6);
+        d100 = MediaPlayer.create(this, R.raw.d100);
+        d2 = MediaPlayer.create(this, R.raw.d2);
+        d10r1 = MediaPlayer.create(this, R.raw.d10r1);
+        d10r2 = MediaPlayer.create(this, R.raw.d10r2);
+        d10r3 = MediaPlayer.create(this, R.raw.d10r3);
+        d12r2 = MediaPlayer.create(this, R.raw.d12r2);
+        d12r3 = MediaPlayer.create(this, R.raw.d12r3);
+        d20_roll1 = MediaPlayer.create(this, R.raw.d20_roll1);
+        d20_roll2 = MediaPlayer.create(this, R.raw.d20_roll2);
+        d20_roll3 = MediaPlayer.create(this, R.raw.d20_roll3);
+        d4r2 = MediaPlayer.create(this, R.raw.d4r2);
+        d6r1 = MediaPlayer.create(this, R.raw.d6r1);
+        d6r2 = MediaPlayer.create(this, R.raw.d6r2);
+        d8r1 = MediaPlayer.create(this, R.raw.d8r1);
+        d8r2 = MediaPlayer.create(this, R.raw.d8r2);
 
         //sensor things
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
@@ -87,60 +90,88 @@ public class RollingDice extends Activity {
         rollButton.setOnClickListener(view -> {
             int rolledNum = die.roll();
             String rolledNumString = String.valueOf(rolledNum);
-
-            if(sides == 6 && rolledNum == 6 && cheat > 0){
-                cheatedD20.start();
-            }
-            else if(rolledNum == sides && cheat > 0){
-                cheatedD20.start();
-            }
-            else if(sides < 3){
-                d2.start();
-            }
-            else if(sides < 5){
-                d4r2.start();
-            }
-            else if(sides < 7){
-                if((int) (Math.random() * 2) == 2){d6r1.start();}
-                else{d6r2.start();}
-            }
-            else if(sides < 9){
-                if((int) (Math.random() * 2) == 2){d8r1.start();}
-                else{d8r2.start();}
-            }
-            else if(sides < 11){
-                int miniRNG = (int) (Math.random() * 3);
-                if(miniRNG == 3){d10r1.start();}
-                else if(miniRNG == 2){d10r3.start();}
-                else{d10r2.start();}
-            }
-            else if(sides < 19){
-                if((int) (Math.random() * 2) == 2){d12r2.start();}
-                else{d12r3.start();}
-            }
-            else if(sides < 21){
-                int miniRNG = (int) (Math.random() * 4);
-                if(miniRNG == 4){d20_roll1.start();}
-                else if(miniRNG == 3){d20_roll2.start();}
-                else if(miniRNG == 2){rockAndRoll.start();}
-                else{d20_roll3.start();}
-            }
-            else{
-                d100.start();
-            }
+            playRollingSound(rolledNum);
             rollButton.setText(rolledNumString);
+            if(rolledNum == 0){no.start();}
         });
     }
 
     @Override
     protected void onPause() {
         sensorManager.unregisterListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+
+            //releases
+        rockAndRoll.release();
+        no.release();
+        cheatedD20.release();
+        cheatedD6.release();
+        d100.release();
+        d2.release();
+        d10r1.release();
+        d10r2.release();
+        d10r3.release();
+        d12r2.release();
+        d12r3.release();
+        d20_roll1.release();
+        d20_roll2.release();
+        d20_roll3.release();
+        d4r2.release();
+        d6r1.release();
+        d6r2.release();
+        d8r1.release();
+        d8r2.release();
+
+            //nulls
+        sensorManager = null;
+        rockAndRoll = null;
+        no = null;
+        cheatedD20 = null;
+        cheatedD6 =null;
+        d100 = null;
+        d2 = null;
+        d10r1 = null;
+        d10r2 = null;
+        d10r3 = null;
+        d12r2 = null;
+        d12r3 = null;
+        d20_roll1 = null;
+        d20_roll2 = null;
+        d20_roll3 = null;
+        d4r2 = null;
+        d6r1 = null;
+        d6r2 = null;
+        d8r1 = null;
+        d8r2 = null;
         super.onPause();
     }
 
     @Override
     protected void onResume() {
+        //shake sensor re-register
         sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+
+        //media player remake
+        rockAndRoll = MediaPlayer.create(this, R.raw.rock_and_roll);
+        no = MediaPlayer.create(this, R.raw.nooooo);
+        cheatedD20 = MediaPlayer.create(this, R.raw.cheatd20);
+        cheatedD6 = MediaPlayer.create(this, R.raw.cheatd6);
+        d100 = MediaPlayer.create(this, R.raw.d100);
+        d2 = MediaPlayer.create(this, R.raw.d2);
+        d10r1 = MediaPlayer.create(this, R.raw.d10r1);
+        d10r2 = MediaPlayer.create(this, R.raw.d10r2);
+        d10r3 = MediaPlayer.create(this, R.raw.d10r3);
+        d12r2 = MediaPlayer.create(this, R.raw.d12r2);
+        d12r3 = MediaPlayer.create(this, R.raw.d12r3);
+        d20_roll1 = MediaPlayer.create(this, R.raw.d20_roll1);
+        d20_roll2 = MediaPlayer.create(this, R.raw.d20_roll2);
+        d20_roll3 = MediaPlayer.create(this, R.raw.d20_roll3);
+        d4r2 = MediaPlayer.create(this, R.raw.d4r2);
+        d6r1 = MediaPlayer.create(this, R.raw.d6r1);
+        d6r2 = MediaPlayer.create(this, R.raw.d6r2);
+        d8r1 = MediaPlayer.create(this, R.raw.d8r1);
+        d8r2 = MediaPlayer.create(this, R.raw.d8r2);
+
+        //needed super
         super.onResume();
     }
 
@@ -165,50 +196,12 @@ public class RollingDice extends Activity {
             float deltaZAcc = Math.abs(zAcc - prevZAcc);
 
             if (deltaXAcc > SHAKE_THRESHOLD || deltaYAcc > SHAKE_THRESHOLD || deltaZAcc > SHAKE_THRESHOLD) {
-                if (!shaking) {int rolledNum = die.roll();
+                if (!shaking) {
+                    int rolledNum = die.roll();
                     String rolledNumString = String.valueOf(rolledNum);
-
-                    if(sides == 6 && rolledNum == 6 && cheat > 0){
-                        cheatedD20.start();
-                    }
-                    else if(rolledNum == sides && cheat > 0){
-                        cheatedD20.start();
-                    }
-                    else if(sides < 3){
-                        d2.start();
-                    }
-                    else if(sides < 5){
-                        d4r2.start();
-                    }
-                    else if(sides < 7){
-                        if((int) (Math.random() * 2) == 2){d6r1.start();}
-                        else{d6r2.start();}
-                    }
-                    else if(sides < 9){
-                        if((int) (Math.random() * 2) == 2){d8r1.start();}
-                        else{d8r2.start();}
-                    }
-                    else if(sides < 11){
-                        int miniRNG = (int) (Math.random() * 3);
-                        if(miniRNG == 3){d10r1.start();}
-                        else if(miniRNG == 2){d10r3.start();}
-                        else{d10r2.start();}
-                    }
-                    else if(sides < 19){
-                        if((int) (Math.random() * 2) == 2){d12r2.start();}
-                        else{d12r3.start();}
-                    }
-                    else if(sides < 21){
-                        int miniRNG = (int) (Math.random() * 4);
-                        if(miniRNG == 4){d20_roll1.start();}
-                        else if(miniRNG == 3){d20_roll2.start();}
-                        else if(miniRNG == 2){rockAndRoll.start();}
-                        else{d20_roll3.start();}
-                    }
-                    else{
-                        d100.start();
-                    }
+                    playRollingSound(rolledNum);
                     rollButton.setText(rolledNumString);
+                    if(rolledNum == 0){no.start();}
                     shaking = true;
                     atRest = false;
                 }
@@ -225,6 +218,49 @@ public class RollingDice extends Activity {
 
         @Override
         public void onAccuracyChanged(Sensor sensor, int i) {
+        }
+    }
+
+    private void playRollingSound(int rolledNum) {
+        if(sides == 6 && rolledNum == 6 && cheat > 0){
+            cheatedD6.start();
+        }
+        else if(rolledNum == sides && cheat > 0){
+            cheatedD20.start();
+        }
+        else if(sides < 3){
+            d2.start();
+        }
+        else if(sides < 5){
+            d4r2.start();
+        }
+        else if(sides < 7){
+            if((int) (Math.random() * 2) + 1 == 2){d6r1.start();}
+            else{d6r2.start();}
+        }
+        else if(sides < 9){
+            if((int) (Math.random() * 2) + 1 == 2){d8r1.start();}
+            else{d8r2.start();}
+        }
+        else if(sides < 11){
+            int miniRNG = (int) (Math.random() * 3);
+            if(miniRNG == 2){d10r1.start();}
+            else if(miniRNG == 1){d10r3.start();}
+            else{d10r2.start();}
+        }
+        else if(sides < 19){
+            if((int) (Math.random() * 2) + 1 == 2){d12r2.start();}
+            else{d12r3.start();}
+        }
+        else if(sides < 21){
+            int miniRNG = (int) (Math.random() * 4);
+            if(miniRNG == 3){d20_roll1.start();}
+            else if(miniRNG == 2){d20_roll2.start();}
+            else if(miniRNG == 1){rockAndRoll.start();}
+            else{d20_roll3.start();}
+        }
+        else{
+            d100.start();
         }
     }
 }
